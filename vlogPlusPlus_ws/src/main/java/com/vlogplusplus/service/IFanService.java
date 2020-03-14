5b3e3b7e05c0b0cbcb041ed9a46aeaf66c56d549
sorrluck <1179476724@qq.com>
2020-03-14 14:38:15 +0800

    upload

---
+++ vlogPlusPlus_ws/src/main/java/com/vlogplusplus/service/IFanService.java
@@ -0,0 +1,11 @@
+package com.vlogplusplus.service;
+
+import com.vlogplusplus.entity.Fan;
+
+import java.util.List;
+
+public interface IFanService {
+    List<Fan> list_fan(int up_id);
+    List<Fan> list_fellow(int fan_id);
+    void add_fan(int fan_id, int up_id);
+}
---
+++ vlogPlusPlus_ws/src/main/java/com/vlogplusplus/service/impl/FanService.java
@@ -0,0 +1,32 @@
+package com.vlogplusplus.service.impl;
+
+
+import com.vlogplusplus.dao.IFanDao;
+import com.vlogplusplus.entity.Fan;
+import com.vlogplusplus.service.IFanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FanService implements IFanService {
+    @Autowired
+    private IFanDao iFanDao;
+
+
+    @Override
+    public List<Fan> list_fan(int up_id) {
+        return iFanDao.list_fan(up_id);
+    }
+
+    @Override
+    public List<Fan> list_fellow(int fan_id) {
+        return iFanDao.list_fellow(fan_id);
+    }
+
+    @Override
+    public void add_fan(int fan_id, int up_id) {
+        iFanDao.add_fan(fan_id,up_id);
+    }
+}

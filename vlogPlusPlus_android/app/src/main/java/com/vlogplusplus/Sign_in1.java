cefadffea90bc3616b9126cf79432dd3302420a2
linjiangyi <837948074@qq.com>
2020-03-20 18:18:11 +0800

    登录和注册界面
    暂时将登陆界面设置成入口界面。
    登陆界面分两个：1-账号密码，2-验证码。
    在color和string增加了一些量。
    在mipmap里传入了图标（均为黑色，后期需要修改颜色）和登陆界面用到的背景。
    最后在drawable里还加入了两个xml格式的背景样式（是用于background）。

---
+++ vlogPlusPlus_android/app/src/main/java/com/vlogplusplus/Sign_in1.java
@@ -0,0 +1,57 @@
+package com.vlogplusplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
+
+public class Sign_in1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in1);
+        //验证码登录
+        TextView tv = findViewById(R.id.verifysignin);
+        final SpannableStringBuilder style = new SpannableStringBuilder();
+        style.append("使用短信验证码登录");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent=new Intent(Sign_in1.this,Sign_in2.class);
+                //        Toast.makeText(SignIn1Activity.this, "账号/密码错误!", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        };
+        style.setSpan(clickableSpan, 0, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(Color.parseColor("#FC8C9E"));
+        style.setSpan(foregroundColorSpan, 0, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
+        tv.setText(style);
+
+        //立即注册
+        TextView tv2 = findViewById(R.id.signup);
+        final SpannableStringBuilder style2 = new SpannableStringBuilder();
+        style2.append("立即注册");
+        ClickableSpan clickableSpan2 = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent=new Intent(Sign_in1.this,Registered.class);
+                startActivity(intent);
+            }
+        };
+        style2.setSpan(clickableSpan2, 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ForegroundColorSpan foregroundColorSpan2 = new ForegroundColorSpan(Color.parseColor("#FC8C9E"));
+        style2.setSpan(foregroundColorSpan2, 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv2.setMovementMethod(LinkMovementMethod.getInstance());
+        tv2.setText(style2);
+    }
+}

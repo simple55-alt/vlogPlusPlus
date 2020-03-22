2ae699d243bb17235ea0198486cdf2dabc807a1c
linjiangyi <837948074@qq.com>
2020-03-22 14:50:34 +0800

    视频详情页面80%
    基本布局和控件内容已经完成。
    按钮事件还没有加入。
    增添了一些临时的图片资源。
    不确定控件：
    进度条：seekbar
    视频播放窗口：videoview

---
+++ vlogPlusPlus_android/app/src/main/java/com/vlogplusplus/VideoDetails_page.java
@@ -0,0 +1,14 @@
+package com.vlogplusplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoDetails_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videodetails_page);
+    }
+}

f24705ba83965f317ee14d21d4ffb3738fbc8aa2
xiangfangsong <cnxfs@qq.com>
2020-03-25 00:04:03 +0800

    update

--- vlogPlusPlus_android/app/src/main/java/com/vlogplusplus/VideoDetails_page.java
+++ vlogPlusPlus_android/app/src/main/java/com/vlogplusplus/VideoDetails_page.java
@@ -56,7 +56,8 @@ public class VideoDetails_page extends AppCompatActivity implements View.OnClick
         EditText seachEditText = (EditText) findViewById(R.id.comment_my);
         seachEditText.setFocusable(false);
         seachEditText.setOnClickListener(this);
-        }
+    }
+
     public void transmitDialog() {
         dialog = new Dialog(this,R.style.ActionSheetDialogStyle);        //填充对话框的布局
         inflate = LayoutInflater.from(this).inflate(R.layout.transmit_dialog, null);        //初始化控件
@@ -118,6 +119,7 @@ public class VideoDetails_page extends AppCompatActivity implements View.OnClick
         dialogWindow.setAttributes(lp);
         dialog.show();//显示对话框
     }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
